8ab3e93abd9eeca19da5f35e335b2745b31c2e5c
natshabat <teximnat@gmail.com>
2022-02-22 21:34:06 +0200

    new hw1 epam

---
+++ epam_hw1_ShabatN.py
@@ -0,0 +1,40 @@
+# TASK 1_ Write a Python-script that performs simple arithmetic operations: '+', '-', '*', '/'.
+# The type of operator and data are set on the command line when the script is run.
+# The script should be launched like this:$ python my_task.py1 * 2
+# Notes:For other operations need to raise NotImplementedError.
+# Use the argparsemodule to parse command line arguments.
+# Your implementation shouldn't require entering any parameters (like -f or --function).
+
+a = float(input("Please, enter a:"))
+b = float(input("Please, enter b:"))
+print('sum a+b:', a+b)
+print('subtraction a-b:', a-b)
+print('multiplication a*b:', a*b)
+print('division a/b:', a/b)
+
+# TASK 2_ Write a Python-script that performs the standard math functions on the data.
+# The name of function and data are set on the command line when the script is run.
+# The script should be launched like this:$ python my_task.py add 1 2
+# Notes:Function names must match the standard math functions from the built-in libraries.
+# The script must raises all happened exceptions.For non-mathematical function need to raise NotImplementedError
+
+import math
+x = float(input("Please, enter number:"))
+y = input('Please, enter the name of function (sqrt, ceil or floor):')
+if y == 'sqrt':
+    print('sqrt of x is:', math.sqrt(x))
+elif y == 'ceil':
+    print('ceil of x is:', math.ceil(x))
+elif y == 'floor':
+    print('floor of x is:', math.floor(x))
+else:
+    print('This function was not developed :) have a nice day!')
+
+# TASK 3_ Write a Python-script that determines whether the input string is the correct entry for the 'formula'
+# according EBNF syntax (without using regular expressions).
+# Formula = Number | (Formula Sign Formula)
+# Sign = '+' | '-'
+# Number = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+
+
+
